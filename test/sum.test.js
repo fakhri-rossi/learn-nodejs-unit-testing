5e77c9338ff0d196602c74412aa03fd80d91189f
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-07 17:58:50 +0700

    Jest config: verbose true -> see detailed unit test individually

--- test/sum.test.js
+++ test/sum.test.js
@@ -4,3 +4,13 @@ test("Test sum function", () => {
   const result = sum(2, 5);
   expect(result).toBe(7);
 });
+
+test("Test sum function 2", () => {
+  const result = sum(2, 5);
+  expect(result).toBe(7);
+});
+
+test("Test sum function 3", () => {
+  const result = sum(2, 5);
+  expect(result).toBe(7);
+});
